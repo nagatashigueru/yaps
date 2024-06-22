44e6513cd252ec31a1e340789ab7fa33bf852f58
shigueru nagata <shiguerunagata@protonmail.com>
2024-06-22 15:22:24 -0400

    Se subieron archivos de ejemplo a examplefiles. Se esta implementando el modulo de lectura de la energia de fermi.

---
+++ yaps-dos/modules/fermi.f95
@@ -0,0 +1,58 @@
+! ****************************************************************************|
+! Nombre      :: fermi                                                        |
+! Author      :: Shigueru Nagata                                              |
+! Fecha       :: Junio 2024                                                   |
+! Descripcion :: Modulo que contiene la subroutina para obtener la energia de |
+!                fermi, desde el archivo de salida (.out) de QuantumESPRESSO  |
+! ****************************************************************************|
+module fermi
+    implicit none
+
+    ! -------------------------------------------------------------------------|
+    ! ~~~~~~~~~~~~~~~~~~~~~~~~ variables - parameters ~~~~~~~~~~~~~~~~~~~~~~~~~|
+    ! -------------------------------------------------------------------------|
+    ! Line   :: Recibe cada una de las lineas leidas del archivo para luego    |
+    !           ser procesada                                                  |
+    ! Patron :: Patron de frase para identificar la linea que contiene la      |
+    !           energia de fermi                                               |
+    ! Flag   :: Flag que indica cuando se hallo la energia de fermi y detiene  |
+    !           el bucle                                                       |
+    ! Find   :: Recibe el valor de la funcion index para luego ser procesado   |
+    ! -------------------------------------------------------------------------|
+
+    integer, parameter, private :: UnitInput = 23
+
+    character(len=19), parameter, private :: Patron = "The fermi energy is"
+
+    character(len=100), private :: Line
+    
+    integer, private :: Flag 
+    integer, private :: Find
+    integer, private :: IOError
+
+    ! *****************
+    ! ... subrutina ...
+    ! *****************
+
+    contains
+
+        subroutine fermi(FileInput, FermiEnergy)
+           
+            implicit none
+
+            character(len=50), intent (in) :: FileInput
+
+            real, intent (out) :: FermiEnergy
+
+            Flag = 0
+
+            do while(Flag .ne. 1)
+                read(unit=UnitInput,file=FileInput,status="old") Line
+                Find = index(Line,Patron)
+                if (Find .ne. 0 and Flag .ne. 1) then
+                    read(Line,fmt=) FermiEnergy
+                    Flag = 1
+
+            end do
+        end subroutine fermi
+end module fermi
