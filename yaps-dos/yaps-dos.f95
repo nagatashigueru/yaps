26cfd09aeaf6d4d87ee3d1636d3b7b71ab4ec54d
shigueru nagata <shiguerunagata@protonmail.com>
2024-06-22 00:50:23 -0400

    Se creo carpeta para yaps-dos Se creo source yaps-dos.f95 que es el main del programa

---
+++ yaps-dos/yaps-dos.f95
@@ -0,0 +1,9 @@
+!*****************************************************************************
+! Nombre      :: yaps-dos
+! Descripcion :: Script para procesamiento de densidades de estado parciales.
+! Autor       :: Shigueru Nagata
+!*****************************************************************************
+
+program DOS
+    implicit none
+end program DOS
